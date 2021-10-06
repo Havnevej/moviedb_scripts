07de4ab3ecec509b0466e557a65aa0bb7115bc1e
Hammi007 <56402346+Hammi007@users.noreply.github.com>
2021-10-06 23:47:53 +0200

    dblink insert data
    dblink insert data from imdb dataset into our database template.

--- scripts/transfer_data.sql
+++ scripts/transfer_data.sql
@@ -2,7 +2,14 @@
 -- create extension dblink 
 
 -- create table table_testt (primary_title varchar(500));
-insert into table_testt
-SELECT primarytitle FROM dblink('dbname=imdb user=postgres password=Amazing1234', 'select primarytitle from title_basics')
-AS tt(primarytitle varchar(500))
-            
+
+--insert into table_testt
+--SELECT primarytitle FROM dblink('dbname=imdb user=postgres password=Amazing1234', 'select primarytitle from title_basics')
+--AS tt(primarytitle varchar(500))
+
+-- Insert in db template
+insert into title(title_id)
+SELECT tconst FROM dblink('dbname=imdb user=postgres password=Amazing1234', 'select tconst from title_basics')
+AS tt(tconst varchar(500))
+              
+						
