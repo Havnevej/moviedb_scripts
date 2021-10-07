003257f6fd51145019ccc75167b31a0d083470d4
adu <adu@nemlig.com>
2021-10-07 12:13:42 +0200

    Setup script for windows
    Signed-off-by: adu <adu@nemlig.com>

--- setup.ps1
+++ setup.ps1
@@ -1,16 +1,29 @@
 $PSQL="C:\Program Files\PostgreSQL\13\bin\psql.exe"
 $env:PGPASSWORD='postgres'
-$order = @('our_datebase.sql', 'add_constraint.sql', 'add_data.sql')
+$env:PGOPTIONS="-c client_min_messages=error"
+$order = @('our_datebase.sql', 'add_consraint.sql', 'add_data.sql')
 $temp_database_name = "imdb"
 $our_database_name = "moviedb"
 $server = "127.0.0.1"
 
+function setup_databases{
+    if($args[0] -ne "keep"){
+        Write-Host "dropping imdb database" -ForegroundColor red
+        & $PSQL -U postgres -h $server  "-c drop database $temp_database_name"
+        Write-Host "creating imdb database" -ForegroundColor red
+        & $PSQL -U postgres -h $server  "-c create database $temp_database_name"
+        Write-Host "restoring imdb database from backup" -ForegroundColor red
+        & $PSQL -U postgres -h $server  -d "$temp_database_name" -q -f "./scripts/imdb_small.backup"
+    }
+    & $PSQL -U postgres -h $server  "-c drop database $our_database_name"
+    & $PSQL -U postgres -h $server  "-c create database $our_database_name"
+}
 
 
-& $PSQL -U postgres -h $server  "-c create database $temp_database_name"
-& $PSQL -U postgres -h $server  "-c create database $our_database_name"
-& $PSQL -U postgres -h $server  -d "$temp_database_name" -f "./scripts/imdb_small.backup"
-
+setup_databases 'keep'
+Write-Host "Running our scripts in order:"  -ForegroundColor green
+Write-Host "[$order]\n\n"  -ForegroundColor yellow
 foreach ($script in $order) {
-    & $PSQL -U postgres -h $server -d "$our_database_name" -f "./scripts/$script"
+    Write-Host "running $script" -ForegroundColor red
+    & $PSQL -U postgres -h $server -d "$our_database_name" -q -f "./scripts/$script" 
 }
