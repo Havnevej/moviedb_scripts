0e16340970ac8d0e75568dbff015948d7b4d9958
Leon <noelem@protonmail.com>
2021-10-07 09:42:47 +0200

    leon

--- scripts/add_data.sql
+++ scripts/add_data.sql
@@ -1,21 +1,29 @@
-INSERT INTO public.Title("title_id")
-SELECT "tconst"  
-FROM title_basic
+--Title,title_basics
+TRUNCATE "public".titletest;
+
+INSERT INTO public.Title("title_id", "title_type", "original_title", 
+"primary_title", "is_adult", "start_year", "end_year", "run_time_minutes" )
+SELECT  "tconst","titletype","originaltitle","primarytitle","isadult","startyear","endyear","runtimeminutes"
+FROM "public".title_basics; 
+
+--Genre_key,
+TRUNCATE "public".Genre_key;
+
+INSERT INTO public.Genre_key("title_id") /*genre_id shold be auto incrementet*/
+SELECT  "tconst"
+FROM "public".title_basics; 
+
+--Genre,
+TRUNCATE "public".Genre;
+
+INSERT INTO public.Genre("genre_id","genre_name")
+SELECT "genre_id", --select with loop, that slits on comma
+FROM "public".Genre_key;
 
 
-INSERT INTO public.titletest("title_type","original_title")
-SELECT "titletype", "originaltitle" 
-FROM "public".title_basics 
+--Title_rating,title_ratings
+TRUNCATE "public".Title_rating;
 
-
-/*
-CREATE TABLE "public".TitleTest(
-    title_id character(20),
-    title_type character(20),
-    original_title character(20),
-    primary_title character(20),
-    is_adult boolean,
-    start_year character(4),
-    end_year character(4),
-    run_time_minutes int4
-);*/
+INSERT INTO public.Title("title_id", "rating_avg", "votes")
+SELECT "tconst", "", ""
+FROM "public".title_ratings;
