b50d943fba43474ce99f11a0bb787f6d51c6023f
Leon <noelem@protonmail.com>
2021-10-08 22:45:50 +0200

    added insert logic for table: genre, knowfortitles

--- scripts/add_data.sql
+++ scripts/add_data.sql
@@ -27,39 +27,42 @@ INSERT INTO public.Episodes("parent_title_id", "title_ud", "season_nr", "episode
 SELECT "parenttconst", "tconst", "seasonnumber", "episodenumber"
 FROM "public".title_episode;
 
--- person 
+--Person 
 INSERT INTO public.person ("person_id", "person_name","birthyear","deathyear") 
 SELECT "nconst", "primaryname", "birthyear","deathyear"
 from "public".name_basics
 
--- genre
-INSERT INTO pulic.genre(title_id,genre_name) 
-SELECT DISTINCT trim(unnest(string_to_array(title_basics.genres, ','))) 
+--Genre
+INSERT INTO public.genre(title_id,genre_name) 
+SELECT tconst, unnest(string_to_array(genres, ','))
 FROM title_basics;
+
+--Profession 
+INSERT INTO public.profession("person_id", "profession_type")
+SELECT nconst, unnest(string_to_array(primaryprofession, ',')) 
+FROM name_basics
+
+--Known_for_titles
+INSERT INTO "public".known_for_titles (person_id, title_id) 
+SELECT nconst, unnest(string_to_array(knownfortitles, ','))
+from name_basics;
+
+
+
+
 
 /*
 -- genre key
 INSERT INTO public.genre_key("title_id", "genre_id")
 SELECT "tconst", "genre_id"
 FROM "public".title, "public".genre
-*/
 
--- profession 
-INSERT INTO public.profession("person_id", "profession_type")
-SELECT nconst, unnest(string_to_array(primaryprofession, ',')) 
-FROM name_basics
-
-/*
 -- profession key
 INSERT INTO public.profession_key("person_id", "profession_id")
 SELECT unnest(string_to_array(primaryprofession, ',')), nconst 
 FROM name_basics
+
+-- Known_for_tiles_key  = not implementet
 */
 
--- Known_for_titles
-INSERT INTO known_for_titles (title_id) 
-select distinct trim(unnest(string_to_array(knownfortitles, ','))) 
-from name_basics;
 
-
--- Known_for_tiles_key
