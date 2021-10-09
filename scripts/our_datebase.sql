a0796eaf461f606a02fe95d806f22c964345bb8a
Leon <noelem@protonmail.com>
2021-10-09 15:38:54 +0200

    comments

--- scripts/our_datebase.sql
+++ scripts/our_datebase.sql
@@ -1,3 +1,4 @@
+/*
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -8,10 +9,9 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
 SET default_tablespace = '';
-
 SET default_table_access_method = heap;
+*/
 
 CREATE TABLE public.Person (
     person_id varchar(255),
