f7f380f4c2c1956e9222841daee7348bba0b831d
Anton D <adu@nemlig.com>
2021-10-10 12:40:02 +0200

    Added the final constraint script

--- scripts/add_consraint.sql
+++ scripts/add_consraint.sql
@@ -1,17 +1,14 @@
-ALTER TABLE ONLY public.omdb_data
-    ADD CONSTRAINT omdb_data_pkey PRIMARY KEY (t_id);
+ALTER TABLE ONLY public.omdb
+    ADD CONSTRAINT omdb_pkey PRIMARY KEY (title_id);
 
 ALTER TABLE ONLY public.word_index
-    ADD CONSTRAINT word_index_pkey PRIMARY KEY (t_id, word, field);
+    ADD CONSTRAINT word_index_pkey PRIMARY KEY (title_id, word, field);
 
 ALTER TABLE ONLY public.crew
     ADD CONSTRAINT crew_pkey PRIMARY KEY (title_id, ordering);
 
-ALTER TABLE ONLY public.principals
-    ADD CONSTRAINT principals_pkey PRIMARY KEY (title_id, ordering);
-
-ALTER TABLE ONLY public.characters
-    ADD CONSTRAINT characters_pkey PRIMARY KEY (character_name, title_id);
+ALTER TABLE ONLY public.character_names
+    ADD CONSTRAINT character_names_pkey PRIMARY KEY (person_id, title_id, character_name);
 
 ALTER TABLE ONLY public.Title_rating
     ADD CONSTRAINT title_rating_pkey PRIMARY KEY (title_id);
@@ -23,16 +20,16 @@ ALTER TABLE ONLY public.Person
     ADD CONSTRAINT person_pkey PRIMARY KEY (person_id);
 
 ALTER TABLE ONLY public.Profession
-    ADD CONSTRAINT profession_pkey PRIMARY KEY (person_id);
+    ADD CONSTRAINT profession_pkey PRIMARY KEY (person_id, profession_type);
 
 ALTER TABLE ONLY public.Known_for_titles
-    ADD CONSTRAINT known_for_titles_pkey PRIMARY KEY (person_id);
+    ADD CONSTRAINT known_for_titles_pkey PRIMARY KEY (person_id, title_id);
 
 ALTER TABLE ONLY public.Title_versions
     ADD CONSTRAINT Title_versions_pkey PRIMARY KEY (title_id,title_version);
 
 ALTER TABLE ONLY public.Genre
-    ADD CONSTRAINT genre_pkey PRIMARY KEY (title_id);
+    ADD CONSTRAINT genre_pkey PRIMARY KEY (title_id, genre_name);
 
 ALTER TABLE ONLY public.Episodes
     ADD CONSTRAINT episodes_pkey PRIMARY KEY (parent_title_id, title_id);
