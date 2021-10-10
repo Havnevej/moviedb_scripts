aa32a7e3c2a5e38e2eee1bdcea863280a5f782cf
Anton D <adu@nemlig.com>
2021-10-10 14:20:41 +0200

    Fixed windows setup script

--- setup.ps1
+++ setup.ps1
@@ -1,29 +1,38 @@
-$PSQL="C:\Program Files\PostgreSQL\13\bin\psql.exe"
+$PSQL=""
 $env:PGPASSWORD='postgres'
 $env:PGOPTIONS="-c client_min_messages=error"
-$order = @('our_datebase.sql', 'add_consraint.sql', 'add_data.sql')
-$temp_database_name = "imdb"
-$our_database_name = "moviedb"
+$order = @("add_database_and_tables.sql", "add_constraints.sql", "add_data.sql", "functions.sql", "final.sql")
+$database_name = "imdb"
 $server = "127.0.0.1"
 
+if(Test-Path -Path "C:\Program Files\PostgreSQL\13\bin\psql.exe"){
+    Write-Host "Using psql ver. 13"
+    $PSQL="C:\Program Files\PostgreSQL\13\bin\psql.exe" #version 13
+} elseif (Test-Path -Path "C:\Program Files\PostgreSQL\14\bin\psql.exe"){
+    Write-Host "Using psql ver. 14"
+    $PSQL="C:\Program Files\PostgreSQL\14\bin\psql.exe" #version 14
+}
+
 function setup_databases{
-    if($args[0] -ne "keep"){
-        Write-Host "dropping imdb database" -ForegroundColor red
-        & $PSQL -U postgres -h $server  "-c drop database $temp_database_name"
-        Write-Host "creating imdb database" -ForegroundColor red
-        & $PSQL -U postgres -h $server  "-c create database $temp_database_name"
-        Write-Host "restoring imdb database from backup" -ForegroundColor red
-        & $PSQL -U postgres -h $server  -d "$temp_database_name" -q -f "./scripts/imdb_small.backup"
+    Write-Host "dropping imdb database" -ForegroundColor red
+    & $PSQL -U postgres -h $server  "-c drop database $database_name WITH (FORCE);"
+    Write-Host "creating imdb database" -ForegroundColor red
+    & $PSQL -U postgres -h $server  "-c create database $database_name"
+    Write-Host "restoring imdb database from backup" -ForegroundColor red
+    & $PSQL -U postgres -h $server  -d "$database_name" -q -f "./scripts/imdb_small.backup"
+}
+function run_scripts{
+    Write-Host "Running our scripts in order:"  -ForegroundColor green
+    Write-Host "[$order]"  -ForegroundColor yellow
+    foreach ($script in $order) {
+        Write-Host "running $script" -ForegroundColor red
+        & $PSQL -U postgres -h $server -d "$database_name" -q -f "./scripts/$script" 
     }
-    & $PSQL -U postgres -h $server  "-c drop database $our_database_name"
-    & $PSQL -U postgres -h $server  "-c create database $our_database_name"
+}
+if($args[0] -eq "ours_only"){
+    run_scripts
+} else {
+    setup_databases
+    run_scripts
 }
 
-
-setup_databases 'keep'
-Write-Host "Running our scripts in order:"  -ForegroundColor green
-Write-Host "[$order]\n\n"  -ForegroundColor yellow
-foreach ($script in $order) {
-    Write-Host "running $script" -ForegroundColor red
-    & $PSQL -U postgres -h $server -d "$our_database_name" -q -f "./scripts/$script" 
-}
