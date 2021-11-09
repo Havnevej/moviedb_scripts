b8453c1b5d1b698102c96c8c22e072be5763192b
Leon <noelem@protonmail.com>
2021-11-09 15:12:58 +0100

    added forignkey: genre

--- scripts/add_constraints.sql
+++ scripts/add_constraints.sql
@@ -32,7 +32,8 @@ ALTER TABLE ONLY public.Title_versions
     ADD CONSTRAINT Title_versions_pkey PRIMARY KEY (title_id,title_version);
 
 ALTER TABLE ONLY public.Genre
-    ADD CONSTRAINT genre_pkey PRIMARY KEY (title_id, genre_name);
+    ADD CONSTRAINT genre_pkey PRIMARY KEY (title_id, genre_name),
+    ADD CONSTRAINT genre_fk FOREIGN KEY(title_id) REFERENCES title(title_id);
 
 ALTER TABLE ONLY public.Episodes
     ADD CONSTRAINT episodes_pkey PRIMARY KEY (parent_title_id, title_id);
